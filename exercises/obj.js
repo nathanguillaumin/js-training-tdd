960908f8e3dfa4e6b6734df654a2b0f0352e0b05
Nathan Guillaumin <nathan.guillaumin@outlook.com>
2020-03-18 10:28:05 +0100

    Basics réalisés

--- exercises/obj.js
+++ exercises/obj.js
@@ -8,6 +8,12 @@
  */
 
 // Your code:
+let obj = {         // creation of an object
+    str: "1337",    // by key "str" store value "1337"
+    num: 42,        // by key "num" store value 42
+    bool: false,    // by key "bool" store value false
+    undefined: undefined,   // by key "undefined" store value undefined
+  };
 
 //* Begin of tests
 const assert = require('assert');
